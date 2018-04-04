fc3743be873aca83019bf1cfe8769fdfd9ef3b5e
xubao <a_xubao@163.com>
2018-04-04 16:34:09 +0800

    2018/04/04 周三

---
+++ java/simpleTest/src/main/java/com/xubao/test/simpleTest/setTest/SetTest1.java
@@ -0,0 +1,75 @@
+package com.xubao.test.simpleTest.setTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author xubao
+ * @version 1.0
+ * @since 2018/4/4
+ */
+public class SetTest1
+{
+
+	static class A{
+		private int i = 3;
+
+		public A(int i)
+		{
+			this.i = i;
+		}
+
+		public int getI()
+		{
+			return i;
+		}
+
+		public void setI(int i)
+		{
+			this.i = i;
+		}
+
+		@Override
+		public boolean equals(Object o)
+		{
+			if(this == o) return true;
+			if(o == null || getClass() != o.getClass()) return false;
+
+			A a = (A)o;
+
+			return i == a.i;
+		}
+
+		@Override
+		public int hashCode()
+		{
+			return i;
+		}
+
+		@Override
+		public String toString()
+		{
+			return "A{" +
+					"i=" + i +
+					'}';
+		}
+	}
+
+	public static void main(String[] args){
+		A a1 = new A(1);
+		A a2 = new A(2);
+		Set<A> aSet = new HashSet<A>();
+		aSet.add(a1);
+		aSet.add(a2);
+		System.out.println(a1.equals(a2));
+		System.out.println("a1="+a1.hashCode());
+		System.out.println("a2="+a2.hashCode());
+		System.out.println(aSet);
+		a2.setI(1);
+		//set不能添加相同元素,但如果元素可变,则能存相同元素
+		System.out.println(a1.equals(a2));
+		System.out.println("a1="+a1.hashCode());
+		System.out.println("a2="+a2.hashCode());
+		System.out.println(aSet);
+	}
+}
---
+++ java/simpleTest/src/main/java/com/xubao/test/simpleTest/toolkitTest/ToolKitTest.java
@@ -0,0 +1,35 @@
+package com.xubao.test.simpleTest.toolkitTest;
+
+
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.plaf.basic.BasicTreeUI;
+import java.awt.*;
+import java.awt.event.AWTEventListener;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author xubao
+ * @version 1.0
+ * @since 2018/3/30
+ */
+public class ToolKitTest
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		int i = 0;
+		System.out.println(0xff);
+		System.out.println(0x00ff);
+		System.out.println(0xff00);
+		//System.out.println(-38<<);
+//		while(i<100000)
+//		{
+//			//获取鼠标位置
+//			//System.out.println(System.currentTimeMillis());
+//			Point point = java.awt.MouseInfo.getPointerInfo().getLocation();
+//			//System.out.println(System.currentTimeMillis());
+//			System.out.println(point.x+":"+point.y);
+//			Thread.sleep(100);
+//		}
+
+	}
+}
